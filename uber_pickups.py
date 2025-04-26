039805e848b250077c5a2d610177a730b40fd067
Voranitha Chaiaroon <voranitha.cha@gmail.com>
2025-04-26 22:32:31 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.figure_factory as ff
+# import plotly.figure_factory as ff
 import plotly.express as px
 
 st.title('Uber pickups in NYC')
